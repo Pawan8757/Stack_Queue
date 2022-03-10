dc0c8dddafbad1ddeb625a9da3bf447c95aafdb3
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 00:43:20 +0530

    adding welcome file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC_2_PeakPop
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the stack problem");
+        }
+    }
+}
