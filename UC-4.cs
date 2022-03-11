9435045e59c003cb7879463cb1a4212c73b68821
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:53:15 +0530

    adding 2nd prblm of dequeue

---
+++ UC-4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QueueProblem2
+
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}
