9f17ab53d8080d6575bc0f97875da18a6d0b77c2
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:58:12 +0530

    adding 1st problem of linkedlist

---
+++ LinkedList_UC1/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDemo
+{
+    public class Node
+    {
+
+        public int data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
---
+++ LinkedList_UC1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List operation");
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
