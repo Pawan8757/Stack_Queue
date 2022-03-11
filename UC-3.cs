572fac695bf8209028b29644aae122510712bb7c
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-11 09:51:45 +0530

    adding 1st prblm of dequeue

---
+++ UC-3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QueueProblem
+
+{
+   public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+
+
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
